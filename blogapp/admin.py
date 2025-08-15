f549c4690ec296e099e54dff589e8c959110c9fb
eng.mahdi.alsholi <eng.mahdi.alsholi@gmail.com>
2025-08-15 18:01:36 +0300

    Intial commit

---
+++ blogapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser,Blog
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):  
+    list_display = ("username","first_name", "last_name", "email", 'bio', 'profile_picture',  'facebook', 'twitter', 'instagram', 'linkedin')
+admin.site.register(CustomUser,CustomUserAdmin)
+
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_draft", "category", "created_at")
+
+admin.site.register(Blog, BlogAdmin)
